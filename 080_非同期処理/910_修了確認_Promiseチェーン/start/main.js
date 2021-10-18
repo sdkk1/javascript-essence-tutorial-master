f993f67a7dffc8caf6084c70f55b5059325e67e3
Keita <fast.break.baske03@gmail.com>
2021-10-19 00:45:37 +0900

    update:セクション10 修了確認回答

--- 080_非同期処理/910_修了確認_Promiseチェーン/start/main.js
+++ 080_非同期処理/910_修了確認_Promiseチェーン/start/main.js
@@ -35,3 +35,42 @@ async function myFetch(fileName) {
 	const json = await response.json();
 	return json;
 }
+
+// 答えを見る前の回答 → Promise.all() で並列処理すべき
+// const getTimeLine = async () => {
+// 	const timeLineTarget = await myFetch('user1.json');
+// 	const friendList = await myFetch(`friendsOf${timeLineTarget.id}.json`);
+// 	let friendInfo = [];
+// 	let messageInfo = [];
+
+// 	console.log(`--${timeLineTarget.name}'s timeline--`);
+// 	for (let i = 0; i < friendList.friendIds.length; i++) {
+// 		friendInfo[i] = await myFetch(`user${friendList.friendIds[i]}.json`); // Promise.all() で並列処理へ
+// 		messageInfo[i] = await myFetch(`message${friendInfo[i].latestMsgId}.json`); // Promise.all() で並列処理へ
+// 		console.log(`${friendInfo[i].name} says: ${messageInfo[i].message}`);
+// 	}
+// }
+
+// getTimeLine();
+
+const getTimeLine = async () => {
+	const timeLineTarget = await myFetch('user1.json');
+	console.log(`--${timeLineTarget.name}'s timeline--`);
+
+	const friendIdsList = await myFetch(`friendsOf${timeLineTarget.id}.json`);
+	const friendList = friendIdsList.friendIds.map(id => myFetch(`user${id}.json`));
+	const friends = await Promise.all(friendList);  // Promise.all() で並列処理
+
+	const messagesList = friends.map(friend => myFetch(`message${friend.latestMsgId}.json`));
+	const messages = await Promise.all(messagesList);  // Promise.all() で並列処理
+
+	friends.forEach((friend) => {
+		messages.forEach((message) => {
+			if(friend.id === message.userId) {
+				console.log(`${friend.name} says: ${message.message}`)
+			}
+		})
+	})
+}
+
+getTimeLine();
